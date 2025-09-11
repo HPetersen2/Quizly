a4921e82e90e276f07e20f325e39252781545af8
HPetersen2 <petermann2@web.de>
2025-09-11 15:26:29 +0200

    add url for register, login, logout and refresh token

---
+++ auth_app/api/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path, include
+from .views import RegistrationView, CookieTokenObtainView, CookieRefreshView, TokenBlacklistView
+
+urlpatterns = [
+    path('register/', RegistrationView.as_view(), name='register-view'),
+    path('login/', CookieTokenObtainView.as_view(), name='login-view'),
+    path('logout/', TokenBlacklistView.as_view(), name='token-blacklist'),
+    path('token/refresh/', CookieRefreshView.as_view(), name='token-refresh'),
+]
