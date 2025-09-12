a66f887b0dd080821e95a936b7b27365513441e8
HPetersen2 <petermann2@web.de>
2025-09-12 14:27:41 +0200

    change model

--- services/transcription_service.py
+++ services/transcription_service.py
@@ -1,7 +1,7 @@
 import whisper
 
 def transcribe_audio(audio_file):
-    model = whisper.load_model("turbo")
+    model = whisper.load_model("medium")
     result = model.transcribe(audio_file)
     print(result["text"])
     return result["text"]
