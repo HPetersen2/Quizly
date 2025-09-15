c93ff4d3770dfe7e38291406f0507d4c82578c0e
HPetersen2 <149525216+HPetersen2@users.noreply.github.com>
2025-09-15 17:31:49 +0200

    merge conflict

--- services/youtube_service.py
+++ services/youtube_service.py
@@ -1,18 +1,22 @@
 import yt_dlp
 import os
 
-def download_audio(url, filename: str = "audiofile.mp3") -> str:
+import yt_dlp
+import os
 
+def download_audio(url) -> str:
     ydl_opts = {
-        "format": "bestaudio/best",
-        "outtmpl": filename,
+        "format": "m4a/bestaudio/best",
+        "outtmpl": "audio.%(ext)s",
         "quiet": True,
         "noplaylist": True,
+        "postprocessors": [{
+            "key": "FFmpegExtractAudio",
+            "preferredcodec": "m4a",
+        }],
     }
 
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-        info = ydl.extract_info(url, download=False)
+        info = ydl.extract_info(url, download=True)
         output_path = ydl.prepare_filename(info)
-        base, ext = os.path.splitext(output_path)
-        final_path = base + ".mp3"
-        return os.path.abspath(final_path)
+        return os.path.abspath(output_path)
