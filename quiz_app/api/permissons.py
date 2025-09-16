2ec654c57438fa50079edba8e48dfb53c4b8d716
HPetersen2 <petermann2@web.de>
2025-09-16 14:17:56 +0200

    add quiz permisson: authenticated with cookie and is owner class

---
+++ quiz_app/api/permissons.py
@@ -0,0 +1,29 @@
+from rest_framework.permissions import BasePermission
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework_simplejwt.exceptions import InvalidToken, TokenError
+
+class IsAuthenticatedFromCookie(BasePermission):
+    def has_permission(self, request, view):
+        jwt_authenticator = JWTAuthentication()
+
+        access_token = request.COOKIES.get('access_token')
+        if not access_token:
+            return False
+
+        try:
+            validated_token = jwt_authenticator.get_validated_token(access_token)
+            user = jwt_authenticator.get_user(validated_token)
+
+            request.user = user
+
+            return user is not None and user.is_authenticated
+
+        except (InvalidToken, TokenError):
+            return False
+        except Exception:
+            return False
+
+class IsQuizOwner(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return request.user == obj.owner
+            
